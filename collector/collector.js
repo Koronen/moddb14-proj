0dc0f995a01399d35bdaa1856f2530a8d128a42d
Victor Koronen <koronen@kth.se>
2014-04-17 12:16:56 +0200

    Insert sample data into MongoDB

--- collector/collector.js
+++ collector/collector.js
@@ -1,3 +1,19 @@
 var dotenv = require('dotenv');
 dotenv.load();
-console.log(process.env.MONGODB_URL)
+
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to Database");
+
+  var document = { user: { location: "Stockholm, Sweden" } };
+
+  db.collection('collector').insert(document, function(err, records) {
+    if (err) {
+      throw err;
+    }
+    console.log("Record added as " + records[0]._id);
+  });
+});
